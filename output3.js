49b3d750975935ec6895ab10d97800968fd877dd
ThomasRubycrust <96175908+ThomasRubycrust@users.noreply.github.com>
2022-09-09 14:13:01 +0200

    Create output3.js

---
+++ output3.js
@@ -0,0 +1,45 @@
+-=- Names Directly -=-
+Esateno Nall
+Ouhi Neamete
+Oneresete Dion
+Eret Emetedall
+Atesous Eme
+Alleremeh Ines
+Eresete Datou
+Outeden Eahin
+Atonenio Nall
+Esenion Eame
+Ere Sehineate
+Ion Atesonen
+Ono Utehinall
+Eas Etetedion
+-=- Names Advanced -=-
+esatenonall => Esateno Nall [5, 0]
+ouhineamete => Ouhi Neamete [2, 0]
+oneresetedion => Oneresete Dion [4, 0]
+eretemetedall => Eret Emetedall [5, 0]
+atesouseme => Atesous Eme [12, 0]
+alleremehines => Alleremeh Ines [8, 0]
+eresetedatou => Eresete Datou [9, 0]
+outedeneahin => Outeden Eahin [8, 0]
+atonenionall => Atonenio Nall [1, 0]
+esenioneame => Esenion Eame [13, 0]
+eresehineate => Ere Sehineate [8, 0]
+ionatesonen => Ion Atesonen [2, 0]
+onoutehinall => Ono Utehinall [4, 0]
+easetetedion => Eas Etetedion [10, 0]
+-=- Segment List -=-
+te: ouhi neameTE, ereT Emetedall, ere sehineaTE, ono uTEhinall, eas eTEtedion
+ea: ouhi nEAmete, outeden EAhin, esenion EAme, ere sehinEAte, EAs etetedion
+es: ESateno nall, atESous eme, alleremeh inES, ESenion eame, ion atESonen
+at: esATeno nall, ATesous eme, eresete dATou, ATonenio nall, ion ATesonen
+en: esatENo nall, outedEN eahin, atonENio nall, esENion eame, ion atesonEN
+me: ouhi neaMEte, eret eMEtedall, atesous eME, allereMEh ines, esenion eaME
+se: onereSEte dion, atesouS Eme, ereSEte datou, ere SEhineate, eaS Etetedion
+ou: OUhi neamete, atesOUs eme, eresete datOU, OUteden eahin, onO Utehinall
+on: esatenO Nall, ONeresete dion, atONenio nall, ion atesONen, ONo utehinall
+ted: onereseTE Dion, eret emeTEDall, ereseTE Datou, ouTEDen eahin, eas eteTEDion
+all: esateno nALL, eret emetedALL, ALLeremeh ines, atonenio nALL, ono utehinALL
+ion: oneresete dION, atonenIO Nall, esenION eame, ION atesonen, eas etetedION
+ere: onEREsete dion, EREt emetedall, allEREmeh ines, EREsete datou, ERE sehineate
+hin: ouHI Neamete, alleremeH INes, outeden eaHIN, ere seHINeate, ono uteHINall
