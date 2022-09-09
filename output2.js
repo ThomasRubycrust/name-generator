140ea3d2a70908b8ea739a974287b21b46a5e036
ThomasRubycrust <96175908+ThomasRubycrust@users.noreply.github.com>
2022-09-09 14:12:43 +0200

    Create output2.js

---
+++ output2.js
@@ -0,0 +1,45 @@
+-=- Names Directly -=-
+Alouhe Hiha
+Andith Itinent
+Are Hahehifor
+Areh Ehihafor
+Alitouf Orin
+Ala Reforesen
+Enalesa Ndit
+And Enarehahe
+Iti Nithesent
+Ine Ntoufores
+Andinou Hiha
+Entithit Ouhi
+Eni Thesarehe
+Andali Thenent
+-=- Names Advanced -=-
+alouhehiha => Alouhe Hiha [1, 0]
+andithitinent => Andith Itinent [3, 0]
+arehahehifor => Are Hahehifor [24, 0]
+arehehihafor => Areh Ehihafor [82, 0]
+alitouforin => Alitouf Orin [5, 0]
+alareforesen => Ala Reforesen [8, 0]
+enalesandit => Enalesa Ndit [4, 0]
+andenarehahe => And Enarehahe [42, 0]
+itinithesent => Iti Nithesent [4, 0]
+inentoufores => Ine Ntoufores [3, 0]
+andinouhiha => Andinou Hiha [8, 0]
+entithitouhi => Entithit Ouhi [4, 0]
+enithesarehe => Eni Thesarehe [8, 0]
+andalithenent => Andali Thenent [0, 0]
+-=- Segment List -=-
+in: andith itINent, alitouf orIN, itI Nithesent, INe ntoufores, andINou hiha
+hi: alouhe HIha, are haheHIfor, areh eHIhafor, andinou HIha, entithit ouHI
+he: alouHE hiha, are haHEhifor, areH Ehihafor, and enarehaHE, eni thesareHE
+es: ala reforESen, enalESa ndit, iti nithESent, ine ntouforES, eni thESarehe
+ha: alouhe hiHA, are HAhehifor, areh ehiHAfor, and enareHAhe, andinou hiHA
+al: ALouhe hiha, ALitouf orin, ALa reforesen, enALesa ndit, andALi thenent
+en: ala reforesEN, ENalesa ndit, and ENarehahe, ENi thesarehe, andali thENent
+it: andith ITinent, alITouf orin, enalesa ndIT, ITi nithesent, entithIT ouhi
+ou: alOUhe hiha, alitOUf orin, ine ntOUfores, andinOU hiha, entithit OUhi
+and: ANDith itinent, enalesA NDit, AND enarehahe, ANDinou hiha, ANDali thenent
+are: ARE hahehifor, AREh ehihafor, alA REforesen, and enAREhahe, eni thesAREhe
+for: are hahehiFOR, areh ehihaFOR, alitouF ORin, ala reFOResen, ine ntouFORes
+ent: andith itinENT, iti nithesENT, inE NToufores, ENTithit ouhi, andali thenENT
+ith: andITH itinent, iti nITHesent, entITHit ouhi, enI THesarehe, andalI THenent
