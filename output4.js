278ff0c54580fe32f175046c0a259824e842c7b8
ThomasRubycrust <96175908+ThomasRubycrust@users.noreply.github.com>
2022-09-09 14:13:16 +0200

    Create output4.js

---
+++ output4.js
@@ -0,0 +1,45 @@
+-=- Names Directly -=-
+Are Teforeris
+Esi Sitherete
+Isithat Esou
+Ith Atomeforou
+Eromete Sest
+Eretist Omese
+Eromesef Orou
+Iseres Eforat
+Eratisi Thou
+Esarese Stou
+Aretites Tere
+Aretifo Rithere
+Ere Saretite
+Ome Tistates
+-=- Names Advanced -=-
+areteforeris => Are Teforeris [8, 0]
+esisitherete => Esi Sitherete [5, 0]
+isithatesou => Isithat Esou [4, 0]
+ithatomeforou => Ith Atomeforou [7, 0]
+erometesest => Eromete Sest [11, 0]
+eretistomese => Eretist Omese [3, 0]
+eromeseforou => Eromesef Orou [5, 0]
+isereseforat => Iseres Eforat [3, 0]
+eratisithou => Eratisi Thou [2, 0]
+esaresestou => Esarese Stou [4, 0]
+aretitestere => Aretites Tere [22, 0]
+aretiforithere => Aretifo Rithere [5, 0]
+eresaretite => Ere Saretite [4, 0]
+ometistates => Ome Tistates [1, 0]
+-=- Segment List -=-
+st: eromete seST, eretiST omese, esarese STou, aretiteS Tere, ome tiSTates
+at: isithAT esou, ith ATomeforou, iseres eforAT, erATisi thou, ome tistATes
+te: are TEforeris, esi sithereTE, eromeTE sest, aretiTEs tere, ere saretiTE
+ou: isithat esOU, ith atomeforOU, eromesef orOU, eratisi thOU, esarese stOU
+ti: ereTIst omese, areTItes tere, areTIfo rithere, ere sareTIte, ome TIstates
+is: are teforerIS, esI Sitherete, ISithat esou, ISeres eforat, eratISi thou
+es: ESi sitherete, isithat ESou, ESarese stou, erE Saretite, ome tistatES
+er: are teforERis, ERomete sest, ERomesef orou, ERatisi thou, ERe saretite
+se: eromete SEst, eretist omeSE, eromeSEf orou, isereS Eforat, esareSE stou
+are: ARE teforeris, esAREse stou, AREtites tere, AREtifo rithere, ere sAREtite
+ith: esi sITHerete, isITHat esou, ITH atomeforou, eratisI THou, aretifo rITHere
+ere: esi sithEREte, EREtist omese, isEREs eforat, aretites tERE, aretifo rithERE
+ome: ith atOMEforou, erOMEte sest, eretist OMEse, erOMEsef orou, OME tistates
+for: are teFOReris, ith atomeFORou, eromeseF ORou, iseres eFORat, aretiFO Rithere
