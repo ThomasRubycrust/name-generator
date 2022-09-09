4997178260772bd3cf0b6879de1747db315d637a
ThomasRubycrust <96175908+ThomasRubycrust@users.noreply.github.com>
2022-09-09 14:12:27 +0200

    Create output1.js

---
+++ output1.js
@@ -0,0 +1,45 @@
+-=- Names Directly -=-
+Eraret Ehiter
+Era Thatethea
+Ere Athareti
+Eratham Ethou
+Outhe Reater
+Ere Aterehi
+Erathir Emeter
+Erathime Rete
+Outhahi Tire
+Ouhitit Ethi
+Eraretha Rethi
+Erahime Titer
+Areterou Thea
+Aremethit Itha
+-=- Names Advanced -=-
+eraretehiter => Eraret Ehiter [3, 0]
+erathatethea => Era Thatethea [1, 0]
+ereathareti => Ere Athareti [11, 0]
+erathamethou => Eratham Ethou [2, 0]
+outhereater => Outhe Reater [5, 0]
+ereaterehi => Ere Aterehi [8, 0]
+erathiremeter => Erathir Emeter [12, 0]
+erathimerete => Erathime Rete [2, 0]
+outhahitire => Outhahi Tire [3, 0]
+ouhititethi => Ouhitit Ethi [1, 0]
+eraretharethi => Eraretha Rethi [1, 0]
+erahimetiter => Erahime Titer [4, 0]
+areterouthea => Areterou Thea [18, 0]
+aremethititha => Aremethit Itha [0, 0]
+-=- Segment List -=-
+er: ERaret ehiter, ERe athareti, outhE Reater, ERe aterehi, ERaretha rethi
+th: era thateTHea, ere aTHareti, eratham eTHou, ouTHe reater, areterou THea
+re: ere ateREhi, erathiR Emeter, erathime REte, outhahi tiRE, eraretha REthi
+ea: era thatethEA, erE Athareti, outhe rEAter, erE Aterehi, areterou thEA
+me: erathaM Ethou, erathir eMEter, erathiME rete, erahiME titer, areMEthit itha
+hi: eraret eHIter, ere atereHI, outhaHI tire, ouHItit ethi, eraHIme titer
+te: erareT Ehiter, era thaTEthea, ere aTErehi, erathime reTE, ouhitiT Ethi
+ou: eratham ethOU, OUthe reater, OUthahi tire, OUhitit ethi, areterOU thea
+ti: ere athareTI, outhahi TIre, ouhiTIt ethi, erahime TIter, aremethiT Itha
+are: erAREt ehiter, ere athAREti, erAREtha rethi, AREterou thea, AREmethit itha
+thi: eraTHIr emeter, eraTHIme rete, ouhitit eTHI, eraretha reTHI, aremeTHIt itha
+tha: era THAtethea, eraTHAm ethou, ouTHAhi tire, erareTHA rethi, aremethit iTHA
+ter: eraret ehiTER, outhe reaTER, erathir emeTER, erahime tiTER, areTERou thea
+era: ERA thatethea, ERAtham ethou, ERAthir emeter, ERAthime rete, ERAhime titer
