aa55e44c11e72a57914e9d056d2dd9cf49d62d3a
ThomasRubycrust <96175908+ThomasRubycrust@users.noreply.github.com>
2022-09-09 14:13:30 +0200

    Create output5.js

---
+++ output5.js
@@ -0,0 +1,45 @@
+-=- Names Directly -=-
+Allinev Eteha
+Era Retiongome
+Evet Iongeaha
+Eatha Rehate
+Arehas Etheve
+Eromehi Ngall
+Ereveh Ingome
+Erallomeh Itio
+Ineralla Reth
+Ineveseh Itio
+Eat Ioseteth
+Ome Tehiseha
+Ina Lleathare
+Eatengi Nse
+-=- Names Advanced -=-
+allineveteha => Allinev Eteha [3, 0]
+eraretiongome => Era Retiongome [6, 0]
+evetiongeaha => Evet Iongeaha [4, 0]
+eatharehate => Eatha Rehate [3, 0]
+arehasetheve => Arehas Etheve [10, 0]
+eromehingall => Eromehi Ngall [4, 0]
+erevehingome => Ereveh Ingome [4, 0]
+erallomehitio => Erallomeh Itio [18, 0]
+inerallareth => Ineralla Reth [4, 0]
+inevesehitio => Ineveseh Itio [12, 0]
+eatioseteth => Eat Ioseteth [3, 0]
+ometehiseha => Ome Tehiseha [11, 0]
+inalleathare => Ina Lleathare [8, 0]
+eatenginse => Eatengi Nse [52, 1]
+-=- Segment List -=-
+ha: allinev eteHA, evet iongeaHA, eatha reHAte, areHAs etheve, ome tehiseHA
+ng: era retioNGome, evet ioNGeaha, eromehi NGall, ereveh iNGome, eateNGi nse
+in: allINev eteha, INeralla reth, INeveseh itio, INa lleathare, eatengI Nse
+th: eaTHa rehate, arehas eTHeve, ineralla reTH, eat ioseteTH, ina lleaTHare
+ea: evet iongEAha, EAtha rehate, EAt ioseteth, ina llEAthare, EAtengi nse
+se: arehaS Etheve, ineveSEh itio, eat ioSEteth, ome tehiSEha, eatengi nSE
+er: ERa retiongome, ERomehi ngall, EReveh ingome, ERallomeh itio, inERalla reth
+hi: eromeHI ngall, ereveH Ingome, erallomeH Itio, ineveseH Itio, ome teHIseha
+te: allinev eTEha, eatha rehaTE, eat ioseTEth, ome TEhiseha, eaTEngi nse
+eve: allinEV Eteha, EVEt iongeaha, arehas ethEVE, erEVEh ingome, inEVEseh itio
+ome: era retiongOME, erOMEhi ngall, ereveh ingOME, erallOMEh itio, OME tehiseha
+are: erA REtiongome, eathA REhate, AREhas etheve, inerallA REth, ina lleathARE
+all: ALLinev eteha, eromehi ngALL, erALLomeh itio, inerALLa reth, inA LLeathare
+tio: era reTIOngome, eveT IOngeaha, erallomeh iTIO, ineveseh iTIO, eaT IOseteth
